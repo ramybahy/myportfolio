6c576a8fda71c920f7926fd944801e84e360071b
ramybahy <ramy.bahy2@gmail.com>
2023-04-04 19:29:36 +0200

    homepage

--- src/modules/home.js
+++ src/modules/home.js
@@ -4,66 +4,57 @@ import { Outlet, Link } from "react-router-dom";
 function Home() {
 
     const myArray = ['accordion', 'imagemodal', 'progressbar', 'responsivenavBarwithicons', 'responsivenav', 'responsivesearchbar', 'searchmenu', 'sidenav', 'slideshow', 'tabsheader', 'tabstable', 'tabs'];
-
-    const myList = myArray.map((item) => <p>{item}</p>)
-
+  const myProjects = [
+   {name:"clock", link:" https://ramybahy.github.io/Clock/"}
+,{name:"Flex Panel Gallery", link: "https://ramyy.github.io/Flex-Panel-Gallery"}
+,{name:"cities and states", link: "https://ramyy.github.io/cities-and-states"}
+,{name:"Draw Canvas", link: "https://ramyy.github.io/Draw-Canvas"}
+,{name:"List Item", link: "https://ramyy.github.io/List-Item"}
+,{name:"Slide Scroll", link: "https://ramyy.github.io/Slide-Scroll"}
+,{name:"Move Shadow", link: "https://ramyy.github.io/Move-Shadow"}
+,{name:"Sort Without Articles", link: "https://ramyy.github.io/Sort-Without-Articles"}
+,{name:"Speech Synthesis", link: "https://ramyy.github.io/Speech-Synthesis"}
+,{name:"sticky nav", link: "https://ramyy.github.io/Sticky-Nav"}
+,{name:"click drag", link: "https://ramyy.github.io/Click-Drag"}
+,{name:"Whack A Mole", link: "https://ramyy.github.io/Whack-A-Mole"}
+  ]
+    const myList = myArray.map((item,i) =><li key={i}><Link className="nav__link" to={item}>{item}</Link></li>)
+  const myProjectslinks = myProjects.map((item,i) =><li key={i}><a className="nav__link" href={item.link}>{item.name}</a></li>)
 
   return (
     <div className="home">
 <div className="home__inner">
   <ul className="home__links">
-<li className="email">
+<li className="email home__link">
      <a aria-label="email" href="mailto:ramy.bahy2@gmail.com?subject=Mail">
-     mail</a>
-
+     </a>
      </li>
-      <li className="github"> <a aria-label="github"  href="https://github.com/ramybahy/">
-      github</a> </li> 
-      <li className="npm"> <a aria-label="linkedin" href="https://www.linkedin.com/in/ramy-walid/">
-      linkedin
+      <li className="github home__link"> <a aria-label="github"  href="https://github.com/ramybahy/">
+      </a> </li> 
+      <li className="linkedin home__link"> <a aria-label="linkedin" href="https://www.linkedin.com/in/ramy-walid/">
       </a> </li> 
       </ul>
   <div className="home__welcome">
     <h1>Welcome.</h1>
-    <p>Hi, I’m @ramybahy
-👀 I’m interested in programming with a specialization in front-end web development
-🌱 I’m currently learning different courses on programming and web development
-💞️ I’m looking to collaborate on ...different freelancing projects in the front-end web development field</p>
+    <p>My name is Ramy Walid
+I have been working as a freelance Software Application Developer for more than two years.
+I have made several projects using HTML, CSS, JavaScript and React.
+<br/>
+<br/>
+I am looking to collaborate on different projects and to provide innovative solutions for clients.</p>
   </div>
   <div className="home__projects">
     <h2>Projects</h2>
     <nav>
         <ul>
-          <li>
-            <Link classNameName="nav__link" to="/">Home</Link>
-          </li>
-          <li>
-            <Link classNameName="nav__link" to="/accordion">accordion</Link>
-          </li>
-          <li>
-            <Link classNameName="nav__link" to="/imagemodal">imagemodal</Link>
-          </li>
-          <li>
-            <Link classNameName="nav__link" to="/progressbar">progressbar</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/responsivenavBarwithicons">responsivenavBarwithicons</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/responsivenav">responsivenav</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/responsivesearchbar">responsivesearchbar</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/searchmenu">searchmenu</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/sidenav">sidenav</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/slideshow">slideshow</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/tabsheader">tabsheader</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/tabstable">tabstable</Link>
-          </li><li>
-            <Link classNameName="nav__link" to="/tabs">tabs</Link>
-          </li>
+        {/* myProjects.map((project)=>{
+                    <li>
+                    <Link className="nav__link" to="/">project</Link>
+                  </li> 
+        }) */}
+        {myProjectslinks}
+
+        {myList}
         </ul>
 
       </nav>
