6357841adc5be1d1d6be9ae7ddda9424a791268b
ramybahy <ramy.bahy2@gmail.com>
2023-05-02 19:59:55 +0300

    fix icons + routing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
 
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/responsive-dropdown" element={<ResponsiveDropdown />} />
--- src/modules/home.js
+++ src/modules/home.js
@@ -6,17 +6,17 @@ function Home() {
     const myArray = ['accordion', 'imagemodal', 'progressbar', 'responsivenavBarwithicons', 'responsivenav', 'responsivesearchbar', 'searchmenu', 'sidenav', 'slideshow', 'tabsheader', 'tabstable', 'tabs'];
   const myProjects = [
    {name:"clock", link:" https://ramybahy.github.io/Clock/"}
-,{name:"Flex Panel Gallery", link: "https://ramyy.github.io/Flex-Panel-Gallery"}
-,{name:"cities and states", link: "https://ramyy.github.io/cities-and-states"}
-,{name:"Draw Canvas", link: "https://ramyy.github.io/Draw-Canvas"}
-,{name:"List Item", link: "https://ramyy.github.io/List-Item"}
-,{name:"Slide Scroll", link: "https://ramyy.github.io/Slide-Scroll"}
-,{name:"Move Shadow", link: "https://ramyy.github.io/Move-Shadow"}
-,{name:"Sort Without Articles", link: "https://ramyy.github.io/Sort-Without-Articles"}
-,{name:"Speech Synthesis", link: "https://ramyy.github.io/Speech-Synthesis"}
-,{name:"sticky nav", link: "https://ramyy.github.io/Sticky-Nav"}
-,{name:"click drag", link: "https://ramyy.github.io/Click-Drag"}
-,{name:"Whack A Mole", link: "https://ramyy.github.io/Whack-A-Mole"}
+,{name:"Flex Panel Gallery", link: "https://ramybahy.github.io/Flex-Panel-Gallery"}
+,{name:"cities and states", link: "https://ramybahy.github.io/cities-and-states"}
+,{name:"Draw Canvas", link: "https://ramybahy.github.io/Draw-Canvas"}
+,{name:"List Item", link: "https://ramybahy.github.io/List-Item"}
+,{name:"Slide Scroll", link: "https://ramybahy.github.io/Slide-Scroll"}
+,{name:"Move Shadow", link: "https://ramybahy.github.io/Move-Shadow"}
+,{name:"Sort Without Articles", link: "https://ramybahy.github.io/Sort-Without-Articles"}
+,{name:"Speech Synthesis", link: "https://ramybahy.github.io/Speech-Synthesis"}
+,{name:"sticky nav", link: "https://ramybahy.github.io/Sticky-Nav"}
+,{name:"click drag", link: "https://ramybahy.github.io/Click-Drag"}
+,{name:"Whack A Mole", link: "https://ramybahy.github.io/Whack-A-Mole"}
   ]
     const myList = myArray.map((item,i) =><li key={i}><Link className="nav__link" to={item}>{item}</Link></li>)
   const myProjectslinks = myProjects.map((item,i) =><li key={i}><a className="nav__link" href={item.link}>{item.name}</a></li>)
@@ -25,19 +25,19 @@ function Home() {
     <div className="home">
 <div className="home__inner">
   <ul className="home__links">
-<li className="email home__link">
-     <a aria-label="email" href="mailto:ramy.bahy2@gmail.com?subject=Mail">
+<li className=" home__link">
+     <a className="email" aria-label="email" href="mailto:ramy.bahy2@gmail.com?subject=Mail">
      </a>
      </li>
-      <li className="github home__link"> <a aria-label="github"  href="https://github.com/ramybahy/">
+      <li className=" home__link"> <a className="github"  aria-label="github"  href="https://github.com/ramybahy/">
       </a> </li> 
-      <li className="linkedin home__link"> <a aria-label="linkedin" href="https://www.linkedin.com/in/ramy-walid/">
+      <li className=" home__link"> <a className="linkedin"  aria-label="linkedin" href="https://www.linkedin.com/in/ramy-walid/">
       </a> </li> 
       </ul>
   <div className="home__welcome">
     <h1>Welcome.</h1>
     <p>My name is Ramy Walid
-I have been working as a freelance Software Application Developer for more than two years.
+I have been working as a freelance front-end developer for more than two years.
 I have made several projects using HTML, CSS, JavaScript and React.
 <br/>
 <br/>
